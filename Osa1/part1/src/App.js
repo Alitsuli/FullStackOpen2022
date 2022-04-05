7f903d5fa122a3cc42fcf23c476506fbcf1c4de0
Ali <ali_reza222@hotmail.com>
2022-04-05 19:18:40 +0300

    kurssitiedot step4

--- Osa1/part1/src/App.js
+++ Osa1/part1/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component }  from 'react';
 
 const Header = (props) => {
-  console.log(props);
   return(
     <div>
       <h1>{props.course}</h1>
@@ -9,53 +8,71 @@ const Header = (props) => {
   )
 }
 
-const Total = (props) => {
+const Part = ({props, exercises}) => {
   return(
     <div>
-      <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+      <p>{props} {exercises}</p>
     </div>
   )
 }
 
-  const Part = ({props, exercises}) => {
-    return(
-      <div>
-        <p>{props} {exercises}</p>
-      </div>
-    )
-  }
 
 const App = () => {
   
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Content = () => {
+
+    let partName = parts.map(nimiElementti =>{
+      return `${nimiElementti.name}`;
+    })
+
+    let partExercises = parts.map(harjoitusElementti =>{
+      return `${harjoitusElementti.exercises}`;
+    })
+
     return(
       <div>
-        <Part props={part1.name} exercises={part1.exercises}/>
-        <Part props={part2.name} exercises={part2.exercises}/>
-        <Part props={part3.name} exercises={part3.exercises}/>
+        <Part props={partName[0]} exercises={partExercises[0]}/>
+        <Part props={partName[1]} exercises={partExercises[1]}/>
+        <Part props={partName[2]} exercises={partExercises[2]}/>
       </div>
     )
   }
 
+  const Total = () => {
+
+    let total = parts.map(harjoitusElementti =>{
+      return harjoitusElementti.exercises;
+    })
+    
+    return(
+      <div>
+        <p>Number of exercises {total[0] + total[1] + total[2]}</p>
+      </div>
+    )
+  }
+
+
   return (
     <div>
       <Header course = {course}/>
-      <Content/>
-      <Total e1= {part1.exercises} e2= {part2.exercises} e3= {part3.exercises}/>
+      <Content parts = {parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
